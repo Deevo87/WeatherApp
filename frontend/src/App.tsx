d6e4b01f7b48f883979e79fef41e45f3ca6fc691
rafal.laskowski02@gmail.com <rafalek_0202@wp.pl>
2024-01-12 00:20:44 +0100

    responsive navbar is working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "./App.css";
 import HomePage from "./model/HomePage/HomePage";
 import CurrentWeatherPage from "./model/CurrentWeatherPage/CurrentWeatherPage";
 import ForecastWeatherPage from "./model/ForecastWeatherPage/ForecastWeatherPage";
+import Navbar from "./model/Navbar/navbar";
 
 function App() {
-  return (
-    <div>
-      <h1>WeatherApp</h1>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/currentWeather" element={<CurrentWeatherPage />} />
-          <Route
-            path="/forecastWeather"
-            element={<ForecastWeatherPage />}
-          ></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/currentWeather" element={<CurrentWeatherPage/>}/>
+                    <Route
+                        path="/forecastWeather"
+                        element={<ForecastWeatherPage/>}
+                    ></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
